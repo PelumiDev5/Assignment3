6c2b3b6f819b61e6b90eb87909cad1d37f1a462b
Pelumi Owoshagba <140672785+PelumiDev5@users.noreply.github.com>
2024-07-31 22:31:08 -0400

    completed assignment 3 code

--- router/upload_router.js
+++ router/upload_router.js
@@ -2,20 +2,15 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const upload = require("../middleware/multerConfig");
+const Image = require("../models/files");
+const multer = require("multer");
+const storage = multer.memoryStorage(); //RAM
+const upload = multer({ storage: storage });
 
 // Upload single file
-router
-  .route("/")
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname, "../views/upload.html"));
-  })
-  .post(upload.single("file"), (req, res) => {
-    if (!req.file) {
-      return res.status(400).send("No file uploaded.");
-    }
-    res.send(`File uploaded successfully: ${req.file.path}`);
-  });
+router.route("/").get((req, res) => {
+  res.sendFile(path.join(__dirname, "../views/upload.html"));
+});
 
 // Upload multiple files
 router
@@ -27,10 +22,24 @@ router
     if (!req.files || req.files.length === 0) {
       return res.status(400).send("No files uploaded.");
     }
-    const filePaths = req.files.map((file) => file.path);
-    res
-      .status(200)
-      .send(`Files uploaded successfully: ${filePaths.join(", ")}`);
+
+    const imagePromises = req.files.map((file) => {
+      const newImage = new Image({
+        filename: file.originalname,
+        contentType: file.mimetype,
+        imageBuffer: file.buffer,
+      });
+      return newImage.save();
+    });
+
+    Promise.all(imagePromises)
+      .then(() => {
+        res.status(200).send("Files uploaded successfully.");
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).send("Error saving files to database.");
+      });
   });
 
 module.exports = router;
